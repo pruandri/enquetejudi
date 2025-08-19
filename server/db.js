a516c315027140e17b195d2be62f110cb4fafc65
pruandri <andrimalalaprudence@gmail.com>
2025-08-19 21:46:21 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,17 +3,9 @@ import { Sequelize } from "sequelize";
 
 // Configuration de la base de données PostgreSQL
 //const sequelize = new Sequelize('postgres://postgres:prudence@localhost:5432/enquetejudiciaire');
+const sequelize = new Sequelize('postgres://postgres:prudence@postgres:5432/enquetejudiciaire');
 //const sequelize = new Sequelize('postgres://postgres:prudence@172.17.0.2:5432/enquetejudiciaire');
 //const sequelize = new Sequelize('postgres://postgres:prudence@172.19.0.3:5432/enquetejudiciaire');
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: process.env.DB_HOST || 'postgres',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'enquetejudiciaire',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'prudence',
-});
-
 
 // Vérification de la connexion
 const testConnection = async () => {
